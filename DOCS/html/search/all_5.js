1e37d1cc5812511527b9f95611a25df9bc9ce88e
its-dax <ddax.kn@gmail.com>
2022-01-18 19:27:57 +0100

    new doxyfile

--- DOCS/html/search/all_5.js
+++ DOCS/html/search/all_5.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['main_0',['main',['../_main_8cpp.html#aa06f5fbe915d0bf149dcf793acea47a1',1,'Main.cpp']]],
   ['main_2ecpp_1',['Main.cpp',['../_main_8cpp.html',1,'']]],
-  ['main_2eh_2',['Main.h',['../_main_8h.html',1,'']]],
-  ['mycomp_3',['mycomp',['../_main_8h.html#aed52c981194b1984c876dac60e0b64ea',1,'Main.h']]]
+  ['main_2eh_2',['Main.h',['../_main_8h.html',1,'']]]
 ];
